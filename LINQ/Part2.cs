5df0dd5fcf946e90678bb9156d37ab3fab36b178
blitzdawolf <botjes2@hotmail.com>
2020-06-18 14:42:38 +0200

    added linq querys

---
+++ LINQ/Part2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LINQ
+{
+    public class Part2
+    {
+        List<int> objects = new List<int>();
+
+        /*
+            new Tuple<int, string>(26, "banaan"),
+            new Tuple<int, string>(11, "rijbaan"),
+            new Tuple<int, string>(11, "lamp"),
+            new Tuple<int, string>(39, "lamp"),
+            new Tuple<int, string>(11, "boom"),
+            new Tuple<int, string>(35, "apple")
+         */
+
+        public Part2()
+        {
+            objects.Add(45);
+            objects.Add(84);
+            objects.Add(10);
+            objects.Add(16);
+            objects.Add(30);
+
+            sort();
+            Console.WriteLine("");
+            sortAddSort();
+        }
+
+        private void sortAddSort()
+        {
+            var result = from num in objects orderby num select num;
+
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+            objects.Add(90);
+            Console.WriteLine("");
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private void sort()
+        {
+            var result = from num in objects orderby num select num;
+
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,7 +6,8 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            Part1 p1 = new Part1();
+            // Part1 p1 = new Part1();
+            Part2 p2 = new Part2();
 
             Console.ReadLine();
         }
